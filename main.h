6c987ccdb71c5b0933b7ba486f8a557e68748248
Miss24Chimboto <annastacia78@gmail.com>
2022-07-12 09:28:54 -0700

    Adding a prototype

--- main.h
+++ main.h
@@ -1,6 +1,19 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+/**
+ * struct print - Structure for types
+ *
+ * @type: type to be printed
+ * @f: function to print
+ *
+ */
+typedef struct print
+{
+	char *type;
+	int (*f)(va_list);
+} print_type;
 
 int _printf(const char *format, ...);
-
 #endif /*MAIN_H*/
