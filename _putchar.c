1d3df29a35e3e297763cc7c0a087aab013623498
Miss24Chimboto <annastacia78@gmail.com>
2022-07-12 09:20:02 -0700

    Fixing errors

--- _putchar.c
+++ _putchar.c
@@ -1,7 +1,11 @@
-#include <unistd.h>
-// _putchar - function to return character to stdout
+#include unistd.h>
+/**
+ *_putchar - function to return character to stdout
+ * @c: type char parameter
+ *
+ * Return: 0
+ */
 int _putchar(char c)
 {
-        return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
-
